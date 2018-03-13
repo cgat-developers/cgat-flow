708dd996084aeff8133fe5d56cd7d6f62cc5b5fa
Acribbs <adam.cribbs@imm.ox.ac.uk>
2018-03-13 11:26:42 +0000

    updated config file

--- doc/conf.py
+++ doc/conf.py
@@ -14,7 +14,8 @@
 
 import sys
 import os
-import sphinx_bootstrap_theme
+
+#import sphinx_bootstrap_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -163,14 +164,15 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
-html_theme = 'bootstrap'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
+'''
 html_theme_options = {
     # Navigation bar title. (Default: ``project`` value)
-    'navbar_title': "CGAT Pipelines",
+    'navbar_title': "CGAT-flow",
 
     # Tab name for entire site. (Default: "Site")
     'navbar_site_name': "Site navigation",
@@ -231,9 +233,9 @@ html_theme_options = {
     # Values: "3" (default) or "2" (in quotes)
     'bootstrap_version': "3",
 }
-
+'''
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path=sphinx_bootstrap_theme.get_html_theme_path()
+#html_theme_path=sphinx_bootstrap_theme.get_html_theme_path()
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".

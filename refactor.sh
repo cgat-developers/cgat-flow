10a39ed80add2f4a2b18d4d21545148b1b8f1875
Andreas Heger <andreas.heger@gmail.com>
2018-04-23 20:20:07 +0100

    {AH} refactoring replacement statements for CGATCore

---
+++ refactor.sh
@@ -0,0 +1,42 @@
+# perl -p -i -e "s/from six import StringIO/import io.StringIO as StringIO/" CGATPipelines/*.py
+# perl -p -i -e "s/import io.StringIO as StringIO/from io import StringIO/" CGATPipelines/*.py
+# perl -p -i -e "s/cachedmethod/cached_method/" CGATPipelines/*.py
+# perl -p -i -e "s/openFile/open_file/" CGATPipelines/*.py
+# perl -p -i -e "s/prettyPercent/pretty_percent/" CGATPipelines/*.py
+# perl -p -i -e "s/prettyPercent/pretty_percent/" CGATPipelines/*.py
+# perl -p -i -e "s/getOutputFile/get_output_file/" CGATPipelines/*.py
+# perl -p -i -e "s/readMap/read_map/" CGATPipelines/*.py
+# perl -p -i -e "s/writeMatrix/write_matrix/" CGATPipelines/*.py
+# perl -p -i -e "s/openOutputFile/open_output_file/" CGATPipelines/*.py
+# perl -p -i -e "s/readList/read_list/" CGATPipelines/*.py 
+# perl -p -i -e "s/openFile/open_file/" CGATPipelines/*.py 
+# perl -p -i -e "s/openFile/open_file/" CGATPipelines/*.py 
+
+# perl -p -i -e "s/getParameters/get_parameters/" CGATPipelines/*.py
+# perl -p -i -e "s/asList/as_list/" CGATPipelines/*.py
+
+# perl -p -i -e "s/P.run\(\)/P.run(statement)/" CGATPipelines/*.py
+# perl -p -i -e "s/peekParameters/peek_parameters/" CGATPipelines/*.py
+
+# perl -p -i -e "s/from future.moves.urllib.request import urlopen/from urllib.request import urlopen/" CGATPipelines/*.py
+
+# perl -p -i -e "s/pipeline_genesets.py/genesets/" CGATPipelines/*.py
+
+# perl -p -i -e "s/getTempFile/get_temp_file/" CGATPipelines/*.py
+# perl -p -i -e "s/getTempDir/get_temp_dir/" CGATPipelines/*.py
+# perl -p -i -e "s/import CGAT.BamTools as BamTools/import CGAT.BamTools.bamtools as BamTools/" CGATPipelines/*.py
+
+# perl -p -i -e "s/getNumLines/get_num_lines/" CGATPipelines/*.py
+# perl -p -i -e "s/isEmpty/is_empty/" CGATPipelines/*.py
+# perl -p -i -e "s/P.touch/IOTools.touch_file/" CGATPipelines/*.py
+# perl -p -i -e "s/substituteParameters/substitute_paramaters/" CGATPipelines/*.py
+# perl -p -i -e "s/isComplete/is_complete/" CGATPipelines/*.py
+# perl -p -i -e "s/zapFile/zap_file/" CGATPipelines/*.py
+# perl -p -i -e "s/isPaired/is_paired/" CGATPipelines/*.py
+# perl -p -i -e "s/mergeAndLoad/merge_and_load/" CGATPipelines/*.py
+# perl -p -i -e "s/concatenateAndLoad/concatenate_and_load/" CGATPipelines/*.py
+# perl -p -i -e "s/toTable/to_table/" CGATPipelines/*.py
+# perl -p -i -e "s/create_view/createView/" CGATPipelines/*.py
+# perl -p -i -e "s/getDatabaseName/get_database_name/" CGATPipelines/*.py
+# perl -p -i -e "s/importFromIterator/load_from_iterator/" CGATPipelines/*.py
+

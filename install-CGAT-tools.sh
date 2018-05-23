c3738ca2906545fe6143c5a824a885b110eb4f37
Sebastian Luna Valero <sebastian.luna.valero@gmail.com>
2018-05-23 14:43:05 +0100

    update help message to activate conda environment

--- install-CGAT-tools.sh
+++ install-CGAT-tools.sh
@@ -396,10 +396,13 @@ if [[ -z ${TRAVIS_INSTALL} ]] ; then
       echo " The code successfully installed!"
       echo
       echo " To activate the CGAT environment type: "
-      echo " $ source $CONDA_INSTALL_DIR/bin/activate $CONDA_INSTALL_ENV"
+      echo " $ source $CONDA_INSTALL_DIR/etc/profile.d/conda.sh $CONDA_INSTALL_ENV"
+      echo " $ conda activate base"
+      echo " $ conda activate $CONDA_INSTALL_ENV"
+      [[ $INSTALL_PRODUCTION ]] && echo " cgatflow --help"
       echo
       echo " To deactivate the environment, use:"
-      echo " $ source deactivate"
+      echo " $ conda deactivate"
       echo
    fi # if-$ conda create
 

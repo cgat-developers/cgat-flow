ac287f743adedf7e8b67a1d8ea6b8de466d54351
Acribbs <a.p.cribbs@gmail.com>
2025-09-22 10:23:06 +0100

    exclude conda directory from import tests

--- tests/test_import.py
+++ tests/test_import.py
@@ -40,7 +40,7 @@ EXPRESSIONS = (
 EXCLUDE = set(('__init__.py', 'version.py', 'cgat.py', 'cgatflow.py', 
                'geneinfo.py', 'MEDIPS_runner.py', 'expression_runner.py',
                'ZINBA_runner.py', 'idr.py', 'pipeline_splicing',
-               'cgat_logfiles2tsv'))
+               'cgat_logfiles2tsv', 'conda'))
 
 
 def check_import(filename, outfile):
